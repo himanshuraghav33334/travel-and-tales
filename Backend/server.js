1b37b6b89626b611872ba164937ab622b63b520b
Shikhar <shikharmishra.cnt@gmail.com>
2023-11-29 12:27:55 +0530

    changed port number

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
